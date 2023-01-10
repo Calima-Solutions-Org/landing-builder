f60f73bd9b5baa875af21a13b2ab2c4734cdeb6e
ismaelCalimaDev <santiago@calimasolutions.com>
2023-01-10 17:52:30 +0000

    Initial commit

---
+++ config/landing-builder.php
@@ -0,0 +1,6 @@
+<?php
+
+// config for Calima/LandingBuilder
+return [
+
+];
--- src/LandingBuilderServiceProvider.php
+++ src/LandingBuilderServiceProvider.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace VendorName\Skeleton;
+namespace Calima\LandingBuilder;
 
 use Filament\PluginServiceProvider;
 use Spatie\LaravelPackageTools\Package;
 
-class SkeletonServiceProvider extends PluginServiceProvider
+class LandingBuilderServiceProvider extends PluginServiceProvider
 {
-    public static string $name = 'skeleton';
+    public static string $name = 'landing-builder';
 
     protected array $resources = [
         // CustomResource::class,
@@ -22,19 +22,20 @@ class SkeletonServiceProvider extends PluginServiceProvider
     ];
 
     protected array $styles = [
-        'plugin-skeleton' => __DIR__.'/../resources/dist/skeleton.css',
+        'plugin-landing-builder' => __DIR__.'/../resources/dist/landing-builder.css',
     ];
 
     protected array $scripts = [
-        'plugin-skeleton' => __DIR__.'/../resources/dist/skeleton.js',
+        'plugin-landing-builder' => __DIR__.'/../resources/dist/landing-builder.js',
     ];
 
     // protected array $beforeCoreScripts = [
-    //     'plugin-skeleton' => __DIR__ . '/../resources/dist/skeleton.js',
+    //     'plugin-landing-builder' => __DIR__ . '/../resources/dist/landing-builder.js',
     // ];
 
     public function configurePackage(Package $package): void
     {
         $package->name(static::$name);
+        $package->hasRoutes(__DIR__ . '/../routes/web.php');
     }
 }
